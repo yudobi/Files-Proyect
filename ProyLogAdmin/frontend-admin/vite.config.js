471006a9cb2cd9aa53a0febb73eb0af7080e6452
jorge <jorgeantonioramirezricardo@gmail.com>
2025-08-24 16:53:41 -0600

    modificando vite config para el react-router-doom

--- ProyLogAdmin/frontend-admin/vite.config.js
+++ ProyLogAdmin/frontend-admin/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
+  server: {
+    open: true
+  }
 })
