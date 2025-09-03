fdae28e028682419ddb2b317c9414ec124f11b57
jorge <jorgeantonioramirezricardo@gmail.com>
2025-09-03 14:40:51 -0600

    setings

--- ProyLogAdmin/backend/backend/settings.py
+++ ProyLogAdmin/backend/backend/settings.py
@@ -65,9 +65,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Configuración de Cloudinary
 ################################################################################################
