afd05a8971e6742c11d99fdab9d4424f73f7abab
jorge <jorgeantonioramirezricardo@gmail.com>
2025-09-12 11:19:51 -0600

    modificando url de media_url

--- ProyLogAdmin/backend/backend/settings.py
+++ ProyLogAdmin/backend/backend/settings.py
@@ -92,7 +92,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 # URL base de los archivos media
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
 
 # Usa s3 para almacenamiento de archivos en producción
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
